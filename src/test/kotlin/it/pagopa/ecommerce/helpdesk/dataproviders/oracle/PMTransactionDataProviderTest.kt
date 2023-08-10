68c60ec31021a32682d12fc1fcf31da8dc1ec777
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2023-08-10 15:58:47 +0200

    feat: add search by user fiscal code (#13)
    * fix(openapi): update openapi with latest modifications
    
    * fix(openapi): update sample code
    
    * fix(openapi): refactoring searching criteria
    
    * fix: statusDetail -> statusDetails
    
    * feat: add exception handling logic
    
    * fix: add junit tests
    
    * fix: add junit tests for coverage
    
    * fix: refactoring
    
    * chore(build): integrate ecommerce commons library
    
    * chore(build): make commons build skipped by default for local build
    
    * fix: restore gradle build
    
    * feat: add query by user email implementation
    
    * feat: query pagination
    
    * feat: query pagination
    
    * feat(PM): search transaction by email
    
    * feat(PM): search transaction by email
    
    * feat(query): create common interface for search
    
    * fix: rebase
    
    * temp
    
    * fix: code smells
    
    * feat: code refactoring
    
    * fix: add junit tests
    
    * fix: type mapping
    
    * feat: add search by user fiscal code
    
    * fix: add tests for coverage
    
    * Update src/main/kotlin/it/pagopa/ecommerce/helpdesk/services/PmService.kt
    
    Co-authored-by: Giovanni Berti <dev.giovanniberti@gmail.com>
    
    * fix(code-review): remove dynamic search type scan
    
    * fix(code-review): refactoring searchCriteria arg
    
    * fix: rename field searchCriteria -> searchParams
    
    * fix: rename field searchCriteria -> searchParams
    
    * fix: use bind parameters
    
    * fix: map invalid search criteria to mono error
    
    * fix: merge suggestion from dependent PR
    
    ---------
    
    Co-authored-by: Giovanni Berti <dev.giovanniberti@gmail.com>

--- src/test/kotlin/it/pagopa/ecommerce/helpdesk/dataproviders/oracle/PMTransactionDataProviderTest.kt
+++ src/test/kotlin/it/pagopa/ecommerce/helpdesk/dataproviders/oracle/PMTransactionDataProviderTest.kt
@@ -27,7 +27,7 @@ class PMTransactionDataProviderTest {
         Hooks.onOperatorDebug()
         StepVerifier.create(
                 pmTransactionDataProvider.totalRecordCount(
-                    HelpdeskTestUtils.buildSearchRequestByUserMail()
+                    HelpdeskTestUtils.buildSearchRequestByUserMail("test@test.it")
                 )
             )
             .expectNext(1)
@@ -35,7 +35,19 @@ class PMTransactionDataProviderTest {
     }
 
     @Test
-    fun `Should count found result for paginated query`() {
+    fun `Should handle no transaction found by email`() {
+        Hooks.onOperatorDebug()
+        StepVerifier.create(
+                pmTransactionDataProvider.totalRecordCount(
+                    HelpdeskTestUtils.buildSearchRequestByUserMail("unknown@test.it")
+                )
+            )
+            .expectNext(0)
+            .verifyComplete()
+    }
+
+    @Test
+    fun `Should count found result for paginated query for email transaction search`() {
         val expectedResponse =
             listOf(
                 TransactionResultDto()
@@ -46,7 +58,7 @@ class PMTransactionDataProviderTest {
                             .surname("surname")
                             .name("name")
                             .username("username")
-                            .authenticationType("Utente non registrato")
+                            .authenticationType("Utente registrato SPID")
                     )
                     .transactionInfo(
                         TransactionInfoDto()
@@ -82,7 +94,89 @@ class PMTransactionDataProviderTest {
             )
         StepVerifier.create(
                 pmTransactionDataProvider.findResult(
-                    searchParams = HelpdeskTestUtils.buildSearchRequestByUserMail(),
+                    searchParams = HelpdeskTestUtils.buildSearchRequestByUserMail("test@test.it"),
+                    pageNumber = 0,
+                    pageSize = 10
+                )
+            )
+            .expectNext(expectedResponse)
+            .verifyComplete()
+    }
+
+    @Test
+    fun `Should count total record successfully for user fiscal code transaction search`() {
+        Hooks.onOperatorDebug()
+        StepVerifier.create(
+                pmTransactionDataProvider.totalRecordCount(
+                    HelpdeskTestUtils.buildSearchRequestByUserFiscalCode("fiscal_code")
+                )
+            )
+            .expectNext(1)
+            .verifyComplete()
+    }
+
+    @Test
+    fun `Should handle no transaction found by user fiscal code`() {
+        Hooks.onOperatorDebug()
+        StepVerifier.create(
+                pmTransactionDataProvider.totalRecordCount(
+                    HelpdeskTestUtils.buildSearchRequestByUserFiscalCode("unknown-fiscal_code")
+                )
+            )
+            .expectNext(0)
+            .verifyComplete()
+    }
+
+    @Test
+    fun `Should count found result for paginated query for user fiscal code transaction search`() {
+        val expectedResponse =
+            listOf(
+                TransactionResultDto()
+                    .userInfo(
+                        UserInfoDto()
+                            .userFiscalCode("fiscal_code")
+                            .notificationEmail("test@test.it")
+                            .surname("surname")
+                            .name("name")
+                            .username("username")
+                            .authenticationType("Utente registrato SPID")
+                    )
+                    .transactionInfo(
+                        TransactionInfoDto()
+                            .creationDate(OffsetDateTime.parse("2018-06-26T17:05:36.232+02:00"))
+                            .status("In attesa mod1")
+                            .statusDetails("Contabilizzato")
+                            .amount(100)
+                            .fee(50)
+                            .grandTotal(150)
+                            .rrn("rrn")
+                            .authotizationCode("auth code")
+                            .paymentMethodName("payment method name")
+                    )
+                    .paymentInfo(
+                        PaymentInfoDto()
+                            .origin("origin")
+                            .subject("/RFB/718173815252003/0.10/TXT/Pagamento di test 20")
+                    )
+                    .paymentDetailInfo(
+                        PaymentDetailInfoDto()
+                            .iuv("000000044060814")
+                            .idTransaction("MIUR20191119222949")
+                            .creditorInstitution("RMIC81500N")
+                            .paFiscalCode("97061100588")
+                    )
+                    .pspInfo(
+                        PspInfoDto()
+                            .pspId("CCRTIT2TCAR")
+                            .businessName("Test")
+                            .idChannel("97735020584_01")
+                    )
+                    .product(ProductDto.PM)
+            )
+        StepVerifier.create(
+                pmTransactionDataProvider.findResult(
+                    searchParams =
+                        HelpdeskTestUtils.buildSearchRequestByUserFiscalCode("fiscal_code"),
                     pageNumber = 0,
                     pageSize = 10
                 )
