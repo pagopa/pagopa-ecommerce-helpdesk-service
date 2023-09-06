d5dc9cdc228bfd1294ad13c235bc085d1cb9bdd0
Fernando-Granato <fernando.granato@pagopa.it>
2023-09-06 11:45:13 +0200

    feat(openapi): added new data provider class for retrieve payment methods information

---
+++ src/main/kotlin/it/pagopa/ecommerce/helpdesk/dataproviders/PaymentMethodDataProvider.kt
@@ -0,0 +1,24 @@
+package it.pagopa.ecommerce.helpdesk.dataproviders
+
+import it.pagopa.generated.ecommerce.helpdesk.model.HelpDeskSearchTransactionRequestDto
+import it.pagopa.generated.ecommerce.helpdesk.model.PmSearchPaymentMethodsRequestDto
+import it.pagopa.generated.ecommerce.helpdesk.model.SearchPaymentMethodResponseDto
+import reactor.core.publisher.Mono
+
+/**
+ * Payment method data provider interface. This interface models common function for payment method
+ * data provider in order to search for payment method given a specific
+ * HelpDeskSearchTransactionRequestDto criteria
+ *
+ * @see HelpDeskSearchTransactionRequestDto
+ */
+interface PaymentMethodDataProvider {
+
+    /**
+     * Perform paginated query for retrieve all payment method information for the given search
+     * criteria
+     */
+    fun findResult(
+        searchParams: PmSearchPaymentMethodsRequestDto
+    ): Mono<SearchPaymentMethodResponseDto>
+}
---
+++ src/main/kotlin/it/pagopa/ecommerce/helpdesk/dataproviders/oracle/PMPaymentMethodsDataProvider.kt
@@ -0,0 +1,68 @@
+package it.pagopa.ecommerce.helpdesk.dataproviders.oracle
+
+import io.r2dbc.spi.ConnectionFactory
+import it.pagopa.ecommerce.helpdesk.dataproviders.PaymentMethodDataProvider
+import it.pagopa.ecommerce.helpdesk.exceptions.InvalidSearchCriteriaException
+import it.pagopa.ecommerce.helpdesk.exceptions.NoResultFoundException
+import it.pagopa.generated.ecommerce.helpdesk.model.*
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.switchIfEmpty
+
+/**
+ * PaymentMethodsDataProvider implementation that search payment methods into PM DB
+ *
+ * @see PMPaymentMethodsDataProvider
+ */
+class PMPaymentMethodsDataProvider(@Autowired private val connectionFactory: ConnectionFactory) :
+    PaymentMethodDataProvider {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    override fun findResult(
+        searchParams: PmSearchPaymentMethodsRequestDto
+    ): Mono<SearchPaymentMethodResponseDto> {
+        val searchCriteriaType = searchParams.type
+        val invalidSearchCriteriaError =
+            Mono.error<SearchPaymentMethodResponseDto>(
+                InvalidSearchCriteriaException(searchCriteriaType, ProductDto.PM)
+            )
+        return when (searchParams) {
+            is SearchPaymentMethodRequestEmailDto ->
+                getResultSetFromQuery(
+                    resultQuery = "",
+                    searchParam = searchParams.userEmail,
+                    searchType = searchCriteriaType
+                )
+            is SearchPaymentMethodRequestFiscalCodeDto ->
+                getResultSetFromQuery(
+                    resultQuery = "",
+                    searchParam = searchParams.userFiscalCode,
+                    searchType = searchCriteriaType
+                )
+            else -> invalidSearchCriteriaError
+        }
+    }
+
+    private fun getResultSetFromQuery(
+        resultQuery: String,
+        searchParam: String,
+        searchType: String
+    ): Mono<SearchPaymentMethodResponseDto> =
+        Flux.usingWhen(
+                connectionFactory.create(),
+                { connection ->
+                    logger.info("Retrieving payment methods from PM database.")
+
+                    Flux.from(
+                        connection.createStatement(resultQuery).bind(0, searchParam).execute()
+                    )
+                },
+                { it.close() }
+            )
+            .collectList()
+            .switchIfEmpty { Mono.error(NoResultFoundException(searchType)) }
+            .map { SearchPaymentMethodResponseDto() }
+}
