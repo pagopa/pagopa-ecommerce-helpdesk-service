31b993b5bada18c63e4d7b037a8f5fa3fdd0d385
Fernando-Granato <fernando.granato@pagopa.it>
2024-01-05 16:15:51 +0100

    feat: added annotation Component into PMPaymentMethodsDataProvider

--- src/main/kotlin/it/pagopa/ecommerce/helpdesk/dataproviders/oracle/PMPaymentMethodsDataProvider.kt
+++ src/main/kotlin/it/pagopa/ecommerce/helpdesk/dataproviders/oracle/PMPaymentMethodsDataProvider.kt
@@ -7,6 +7,7 @@ import it.pagopa.ecommerce.helpdesk.utils.resultToPaymentMethodDtoList
 import it.pagopa.generated.ecommerce.helpdesk.model.*
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
@@ -15,6 +16,7 @@ import reactor.core.publisher.Mono
  *
  * @see PMPaymentMethodsDataProvider
  */
+@Component
 class PMPaymentMethodsDataProvider(@Autowired private val connectionFactory: ConnectionFactory) :
     PaymentMethodDataProvider {
 
