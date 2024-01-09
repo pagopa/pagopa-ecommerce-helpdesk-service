3c0cb627a1cf57f8eb7606409432a4640e00ef71
Fernando-Granato <117268775+Fernando-Granato@users.noreply.github.com>
2024-01-09 11:16:31 +0100

    feat(helpdesk) CHK-1865 implemented business logic for retrieve user wallet by fiscal code or email (#23)
    * feat(openapi): added new api for search payment methods by input parameter
    
    * feat(openapi): added new data provider class for retrieve payment methods information
    
    * feat(openapi): added controller and service for new endpoint
    
    * feat(openapi): added controller new endpoint /helpdesk/searchPaymentMethods
    
    * feat(openapi): modified openapi with all detail type response
    
    * feat(query): added new query to execute
    
    * feat(logic): added logic for extract value from query
    
    * feat: temp commit
    
    * fix: refactoring code
    
    * fix: query parameter and sort final payment method list by type
    
    * feat: added controller and service business logic
    
    * feat: added tests for controller class
    
    * feat: added script for execute data provider tests
    
    * fix: mocked insert timestamps
    
    * fix: db operation reduce
    
    * fix: db insert for wallet
    
    * feat: added tests for new data provider
    
    * feat: added fiscal code tests for new data provider
    
    * feat: added annotation Component into PMPaymentMethodsDataProvider
    
    * feat: replaced SearchPaymentMethods to SearchPaymentMethod
    
    * feat: added api to UAT postman collection
    
    * fix: javadoc
    
    * fix: removed query comment
    
    * fix: modified openapi description
    
    * fix: modified openapi description
    
    ---------
    
    Co-authored-by: Pietro Tota <pietro.tota@pagopa.it>

---
+++ src/test/kotlin/it/pagopa/ecommerce/helpdesk/dataproviders/oracle/PMPaymentMethodsDataProviderTest.kt
@@ -0,0 +1,179 @@
+package it.pagopa.ecommerce.helpdesk.dataproviders.oracle
+
+import io.r2dbc.h2.H2ConnectionConfiguration
+import io.r2dbc.h2.H2ConnectionFactory
+import it.pagopa.ecommerce.helpdesk.exceptions.NoResultFoundException
+import it.pagopa.generated.ecommerce.helpdesk.model.*
+import java.time.OffsetDateTime
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Hooks
+import reactor.test.StepVerifier
+
+class PMPaymentMethodsDataProviderTest {
+    private val connectionFactory =
+        H2ConnectionFactory(
+            H2ConnectionConfiguration.builder()
+                .inMemory("...")
+                .option("INIT=runscript from './src/test/resources/h2scriptFile.sql'")
+                .build()
+        )
+
+    private val pmPaymentMethodDataProvider = PMPaymentMethodsDataProvider(connectionFactory)
+
+    @Test
+    fun `Should find wallets for an user searching by email`() {
+        val expectedResponse =
+            SearchPaymentMethodResponseDto()
+                .name("name")
+                .fiscalCode("fiscal_code")
+                .notificationEmail("test@test.it")
+                .surname("surname")
+                .username("username")
+                .status("Utente registrato SPID")
+                .addPaymentMethodsItem(
+                    BancomatDetailInfoDto()
+                        .type(DetailTypeDto.BANCOMAT.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .bancomatNumber("cardPartialNumber")
+                        .bancomatAbi("abi")
+                )
+                .addPaymentMethodsItem(
+                    BankAccountDetailInfoDto()
+                        .type(DetailTypeDto.BANK_ACCOUNT.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .bankName("alias")
+                        .bankState("state")
+                )
+                .addPaymentMethodsItem(
+                    BpayDetailInfoDto()
+                        .type(DetailTypeDto.BPAY.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .bpayPhoneNumber("cellPhoneNumber")
+                        .bpayName("bankName")
+                        .idPsp("CCRTIT2TCAR")
+                )
+                .addPaymentMethodsItem(
+                    CardDetailInfoDto()
+                        .type(DetailTypeDto.CARD.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .idPsp("CCRTIT2TCAR")
+                        .cardBin("1234")
+                        .cardNumber("*************1234")
+                )
+                .addPaymentMethodsItem(
+                    GenericMethodDetailInfoDto()
+                        .type(DetailTypeDto.GENERIC_METHOD.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .description("Generic instrument")
+                )
+                .addPaymentMethodsItem(
+                    PaypalDetailInfoDto()
+                        .type(DetailTypeDto.PAYPAL.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .ppayEmail("test@test.it")
+                )
+                .addPaymentMethodsItem(
+                    SatispayDetailInfoDto()
+                        .type(DetailTypeDto.SATISPAY.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .idPsp("CCRTIT2TCAR")
+                )
+
+        val searchParam =
+            SearchPaymentMethodRequestEmailDto().type("USER_EMAIL").userEmail("test@test.it")
+        StepVerifier.create(pmPaymentMethodDataProvider.findResult(searchParam))
+            .assertNext { assertEquals(expectedResponse, it) }
+            .verifyComplete()
+    }
+
+    @Test
+    fun `Should find wallets for an user searching by fiscal code`() {
+        val expectedResponse =
+            SearchPaymentMethodResponseDto()
+                .name("name")
+                .fiscalCode("fiscal_code")
+                .notificationEmail("test@test.it")
+                .surname("surname")
+                .username("username")
+                .status("Utente registrato SPID")
+                .addPaymentMethodsItem(
+                    BancomatDetailInfoDto()
+                        .type(DetailTypeDto.BANCOMAT.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .bancomatNumber("cardPartialNumber")
+                        .bancomatAbi("abi")
+                )
+                .addPaymentMethodsItem(
+                    BankAccountDetailInfoDto()
+                        .type(DetailTypeDto.BANK_ACCOUNT.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .bankName("alias")
+                        .bankState("state")
+                )
+                .addPaymentMethodsItem(
+                    BpayDetailInfoDto()
+                        .type(DetailTypeDto.BPAY.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .bpayPhoneNumber("cellPhoneNumber")
+                        .bpayName("bankName")
+                        .idPsp("CCRTIT2TCAR")
+                )
+                .addPaymentMethodsItem(
+                    CardDetailInfoDto()
+                        .type(DetailTypeDto.CARD.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .idPsp("CCRTIT2TCAR")
+                        .cardBin("1234")
+                        .cardNumber("*************1234")
+                )
+                .addPaymentMethodsItem(
+                    GenericMethodDetailInfoDto()
+                        .type(DetailTypeDto.GENERIC_METHOD.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .description("Generic instrument")
+                )
+                .addPaymentMethodsItem(
+                    PaypalDetailInfoDto()
+                        .type(DetailTypeDto.PAYPAL.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .ppayEmail("test@test.it")
+                )
+                .addPaymentMethodsItem(
+                    SatispayDetailInfoDto()
+                        .type(DetailTypeDto.SATISPAY.value)
+                        .creationDate(OffsetDateTime.parse("2000-10-22T19:03:25.979+02:00"))
+                        .idPsp("CCRTIT2TCAR")
+                )
+
+        val searchParam =
+            SearchPaymentMethodRequestFiscalCodeDto()
+                .type("USER_FISCAL_CODE")
+                .userFiscalCode("fiscal_code")
+        StepVerifier.create(pmPaymentMethodDataProvider.findResult(searchParam))
+            .assertNext { assertEquals(expectedResponse, it) }
+            .verifyComplete()
+    }
+
+    @Test
+    fun `Should not find wallets for an user searching by unknown email`() {
+        Hooks.onOperatorDebug()
+        val searchParam =
+            SearchPaymentMethodRequestEmailDto().type("USER_EMAIL").userEmail("test@notexist.it")
+        StepVerifier.create(pmPaymentMethodDataProvider.findResult(searchParam))
+            .expectError(NoResultFoundException::class.java)
+            .verify()
+    }
+
+    @Test
+    fun `Should not find wallets for an user searching by unknown fiscalCode`() {
+        Hooks.onOperatorDebug()
+        val searchParam =
+            SearchPaymentMethodRequestFiscalCodeDto()
+                .type("USER_FISCAL_CODE")
+                .userFiscalCode("unknownFiscalCode")
+        StepVerifier.create(pmPaymentMethodDataProvider.findResult(searchParam))
+            .expectError(NoResultFoundException::class.java)
+            .verify()
+    }
+}
