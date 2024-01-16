7a309270c9075ab25a5abed2245bc284d9f6daa0
Fernando-Granato <fernando.granato@pagopa.it>
2024-01-16 15:50:50 +0100

    feat: added test for context load

---
+++ src/test/kotlin/it/pagopa/ecommerce/helpdesk/PagopaEcommerceHelpdeskServiceApplicationKtTest.kt
@@ -0,0 +1,17 @@
+package it.pagopa.ecommerce.helpdesk
+
+import org.junit.Assert
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
+
+@SpringBootTest
+@TestPropertySource(locations = ["classpath:application.properties"])
+class PagopaEcommerceHelpdeskServiceApplicationKtTest {
+
+    @Test
+    fun contextLoads() {
+        Assert.assertTrue(true)
+    }
+
+}
