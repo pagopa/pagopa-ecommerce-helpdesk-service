4d73b31dc780dff86986b20d06e19c091409318b
Francesco Di Tonno <francesco.ditonno@bvtech.com>
2024-10-29 16:00:10 +0100

    fix: connection update to multiple databases with a single client

--- src/test/kotlin/it/pagopa/ecommerce/helpdesk/configurations/MongoEcommerceConfigTest.kt
+++ src/test/kotlin/it/pagopa/ecommerce/helpdesk/configurations/MongoEcommerceConfigTest.kt
@@ -9,7 +9,8 @@ class MongoEcommerceConfigTest {
     private val mongoEcommerceConfig = MongoEcommerceConfig()
     private val uri = "mongodb://localhost:27017/test"
     private val mongoClient: MongoClient = mongoEcommerceConfig.ecommerceReactiveMongoClient(uri)
-    val database = "ecommerce"
+    val database_ecommerce = "ecommerce"
+    val database_ecommerce_history = "ecommerce-history"
 
     @Test
     fun `ecommerceReactiveMongoClient should create MongoClient with correct settings`() {
@@ -19,10 +20,23 @@ class MongoEcommerceConfigTest {
     @Test
     fun `ecommerceReactiveMongoTemplate should create ReactiveMongoTemplate with correct client and database`() {
         val reactiveMongoTemplate =
-            mongoEcommerceConfig.ecommerceReactiveMongoTemplate(mongoClient, database)
+            mongoEcommerceConfig.ecommerceReactiveMongoTemplate(mongoClient, database_ecommerce)
 
         assertThat(reactiveMongoTemplate).isNotNull
         assertThat(reactiveMongoTemplate.mongoDatabaseFactory.mongoDatabase.block()?.name)
-            .isEqualTo(database)
+            .isEqualTo(database_ecommerce)
+    }
+
+    @Test
+    fun `ecommerceHistoryReactiveMongoTemplate should create ReactiveMongoTemplate with correct client and database`() {
+        val reactiveMongoTemplate =
+            mongoEcommerceConfig.ecommerceHistoryReactiveMongoTemplate(
+                mongoClient,
+                database_ecommerce_history
+            )
+
+        assertThat(reactiveMongoTemplate).isNotNull
+        assertThat(reactiveMongoTemplate.mongoDatabaseFactory.mongoDatabase.block()?.name)
+            .isEqualTo(database_ecommerce_history)
     }
 }
