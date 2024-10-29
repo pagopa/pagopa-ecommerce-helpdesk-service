e15f8cf7a94ce03b44db86f9896f322c603110e4
FrancescoDiTonno-BVT <francesco.ditonno@bvtech.com>
2024-10-29 17:01:17 +0100

    [PPABV-34] feat: pm database connection configured (#139)
    * feat: pm database connection configured
    
    * feat: new connection to the second db has been estabilished
    
    * fix: connection update to multiple databases with a single client

--- src/main/kotlin/it/pagopa/ecommerce/helpdesk/PagopaEcommerceHelpdeskServiceApplication.kt
+++ src/main/kotlin/it/pagopa/ecommerce/helpdesk/PagopaEcommerceHelpdeskServiceApplication.kt
@@ -1,10 +1,13 @@
 package it.pagopa.ecommerce.helpdesk
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration
 import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(exclude = [MongoReactiveAutoConfiguration::class])
+@SpringBootApplication(
+    exclude = [MongoReactiveAutoConfiguration::class, MongoReactiveDataAutoConfiguration::class]
+)
 class PagopaEcommerceHelpdeskServiceApplication
 
 fun main(args: Array<String>) {
