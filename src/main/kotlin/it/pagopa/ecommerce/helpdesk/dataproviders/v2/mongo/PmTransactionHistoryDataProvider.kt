082509fc09527115513afaa46a2e97730eed8e8f
Alex Mannia <alex.mannia@bvtech.com>
2024-10-31 11:06:02 +0100

    fix: add name for PmTransactionHistoryDataProvider V2 component

--- src/main/kotlin/it/pagopa/ecommerce/helpdesk/dataproviders/v2/mongo/PmTransactionHistoryDataProvider.kt
+++ src/main/kotlin/it/pagopa/ecommerce/helpdesk/dataproviders/v2/mongo/PmTransactionHistoryDataProvider.kt
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
-@Component
+@Component("PmTransactionHistoryDataProviderV2")
 class PmTransactionHistoryDataProvider(
     @Autowired private val pmTransactionsRepository: PmTransactionsRepository
 ) : TransactionDataProvider {
