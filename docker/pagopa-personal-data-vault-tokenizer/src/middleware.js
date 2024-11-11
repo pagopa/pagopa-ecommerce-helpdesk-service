e38eeafdabcc567e9c4f9895808515a405dca009
dylantangredi-bvt <dylan.tangredi@bvtech.com>
2024-11-11 19:45:29 +0100

    [PPABV-42] fix: add missing search transaction test APIs to dev collection (#145)
    * fix: add missing search transaction by email or fiscal code to dev test api; rename new local and dev test api to match existing uat ones
    
    * feat: add test APIs for HTTP error status codes 400/404
    
    * fix: rewrite paths on dev collections from local to remote ones
    
    ---------
    
    Co-authored-by: Simone infante <52280205+infantesimone@users.noreply.github.com>

--- docker/pagopa-personal-data-vault-tokenizer/src/middleware.js
+++ docker/pagopa-personal-data-vault-tokenizer/src/middleware.js
@@ -1,4 +1,10 @@
 module.exports = (req, res, next) => {
+  const db = req.app.db
+  if (req.body.pii === 'notfound@example.com') {
+    res
+    .status(200)
+    .jsonp(db.get("tokens-not-found"));
+  }
   req.method = 'GET'
   next()
 }
