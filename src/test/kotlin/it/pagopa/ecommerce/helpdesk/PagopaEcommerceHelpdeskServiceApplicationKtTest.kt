501fc0841ee5a0d42f7ba59738064233891e3716
Alex Mannia <123939805+a-mannia@users.noreply.github.com>
2024-11-21 18:34:22 +0100

    [PPABV-66] Add psp-api key NPG configuration (#159)
    * feat: add psp-api key NPG configuration
    
    * fix: update README.md
    
    ---------
    
    Co-authored-by: Alex Mannia <alex.mannia@bvtech.com>

--- src/test/kotlin/it/pagopa/ecommerce/helpdesk/PagopaEcommerceHelpdeskServiceApplicationKtTest.kt
+++ src/test/kotlin/it/pagopa/ecommerce/helpdesk/PagopaEcommerceHelpdeskServiceApplicationKtTest.kt
@@ -1,15 +1,12 @@
 package it.pagopa.ecommerce.helpdesk
 
-import it.pagopa.ecommerce.helpdesk.configurations.TracingConfigTest
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
 import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
 @TestPropertySource(locations = ["classpath:application.properties"])
-@Import(TracingConfigTest::class)
 class PagopaEcommerceHelpdeskServiceApplicationKtTest {
 
     @Test
