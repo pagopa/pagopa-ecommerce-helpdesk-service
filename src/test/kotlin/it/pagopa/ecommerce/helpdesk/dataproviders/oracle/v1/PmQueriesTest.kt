e4cedc35c96ee8e2c01e9d191b0bd33e3c7daa49
dylantangredi-bvt <dylan.tangredi@bvtech.com>
2024-11-29 11:35:17 +0100

    [PPABV-75] feat: add date filter queries for pm transactions search (#166)
    * [PPABV-75] feat: add search and count queries for pm transactions through date and time range filter
    
    * [PPABV-75] feat: add unit tests for search and count queries
    
    ---------
    
    Co-authored-by: Simone infante <52280205+infantesimone@users.noreply.github.com>

---
+++ src/test/kotlin/it/pagopa/ecommerce/helpdesk/dataproviders/oracle/v1/PmQueriesTest.kt
@@ -0,0 +1,76 @@
+package it.pagopa.ecommerce.helpdesk.dataproviders.oracle.v1
+
+import it.pagopa.ecommerce.helpdesk.dataproviders.v1.oracle.timestampRangeCountQuery
+import it.pagopa.ecommerce.helpdesk.dataproviders.v1.oracle.timestampRangePaginatedQuery
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class PmQueriesTest {
+
+    @Test
+    fun `timestampRangePaginatedQuery should be properly constructed and trimmed`() {
+        val expectedQuery =
+            """
+       SELECT  
+           pu.FISCAL_CODE, 
+           pu.NOTIFICATION_EMAIL, 
+           pu.SURNAME, 
+           pu.NAME, 
+           pu.USERNAME, 
+           pu.STATUS,
+           pt.CREATION_DATE, 
+           pt.STATUS,
+           pt.ACCOUNTING_STATUS,
+           PP.ORIGIN,
+           pt.AMOUNT, 
+           pt.FEE, 
+           pt.GRAND_TOTAL,
+           pt.rrn,  
+           pt.AUTHORIZATION_CODE,
+           pt.SERVICE_NAME,
+           pp.SUBJECT,
+           ppd.IUV, 
+           ppd.CCP, 
+           ppd.ENTE_BENEFICIARIO, 
+           ppd.ID_DOMINIO,
+           pp2.ID_PSP, 
+           pp2.BUSINESS_NAME, 
+           pp2.ID_CHANNEL
+       FROM AGID_USER.PP_PAYMENT pp
+       LEFT JOIN AGID_USER.PP_TRANSACTION pt 
+           ON pt.FK_PAYMENT = pp.ID 
+       LEFT JOIN AGID_USER.PP_USER pu 
+           ON pu.ID_USER = pt.FK_USER 
+       LEFT JOIN AGID_USER.PP_PAYMENT_DETAIL ppd 
+           ON pp.ID = ppd.PAYMENT_ID 
+       LEFT JOIN AGID_USER.PP_PSP pp2 
+           ON pt.FK_PSP = pp2.ID 
+       WHERE pp.CREATION_DATE BETWEEN ? AND ?
+       OFFSET ? ROWS FETCH NEXT ? ROWS ONLY
+   """
+                .trimIndent()
+
+        assertEquals(expectedQuery, timestampRangePaginatedQuery)
+    }
+
+    @Test
+    fun `timestampRangeCountQuery should be properly constructed and trimmed`() {
+        val expectedQuery =
+            """
+        SELECT COUNT(*)
+        FROM AGID_USER.PP_PAYMENT pp
+        LEFT JOIN AGID_USER.PP_TRANSACTION pt 
+            ON pt.FK_PAYMENT = pp.ID 
+        LEFT JOIN AGID_USER.PP_USER pu 
+            ON pu.ID_USER = pt.FK_USER 
+        LEFT JOIN AGID_USER.PP_PAYMENT_DETAIL ppd 
+            ON pp.ID = ppd.PAYMENT_ID 
+        LEFT JOIN AGID_USER.PP_PSP pp2 
+            ON pt.FK_PSP = pp2.ID 
+        WHERE pp.CREATION_DATE BETWEEN ? AND ?
+    """
+                .trimIndent()
+
+        assertEquals(expectedQuery, timestampRangeCountQuery)
+    }
+}
