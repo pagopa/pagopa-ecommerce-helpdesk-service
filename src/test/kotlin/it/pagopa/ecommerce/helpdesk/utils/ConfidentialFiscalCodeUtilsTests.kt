8537dbc1ebf2e5f0f0966895d92e3be9c5e3de60
e.serrao <emanuele.serrao@bvtech.com>
2025-04-08 15:59:01 +0200

    added tests for fiscal code

---
+++ src/test/kotlin/it/pagopa/ecommerce/helpdesk/utils/ConfidentialFiscalCodeUtilsTests.kt
@@ -0,0 +1,76 @@
+package it.pagopa.ecommerce.helpdesk.utils
+
+import it.pagopa.ecommerce.commons.domain.Confidential
+import it.pagopa.ecommerce.commons.domain.FiscalCode
+import it.pagopa.ecommerce.commons.utils.ConfidentialDataManager
+import it.pagopa.ecommerce.commons.v1.TransactionTestUtils
+import java.util.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.*
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+
+class ConfidentialFiscalCodeUtilsTests {
+
+    private val fiscalCodeCachedMap: MutableMap<String, FiscalCode> = mutableMapOf()
+
+    private val confidentialDataManager: ConfidentialDataManager = mock()
+
+    private val confidentialFiscalCodeUtils: ConfidentialFiscalCodeUtils =
+        ConfidentialFiscalCodeUtils(confidentialDataManager, fiscalCodeCachedMap)
+
+    @Test
+    fun shouldDecryptFiscalCodeSuccessfully() {
+        val fiscalCode = FiscalCode(TransactionTestUtils.EMAIL_STRING)
+        val fiscalCodeToken = UUID.randomUUID()
+        val computedConfidential = Confidential<FiscalCode>(fiscalCodeToken.toString())
+
+        /* preconditions */
+        given(confidentialDataManager.decrypt(eq(computedConfidential), any()))
+            .willReturn(Mono.just(fiscalCode))
+
+        /* test */
+        StepVerifier.create(confidentialFiscalCodeUtils.toFiscalCode(computedConfidential))
+            .expectNext(fiscalCode)
+            .verifyComplete()
+
+        assertEquals(fiscalCode, fiscalCodeCachedMap[computedConfidential.opaqueData])
+        verify(confidentialDataManager, Mockito.times(1)).decrypt(eq(computedConfidential), any())
+    }
+
+    @Test
+    fun shouldDecryptFiscalCodeSuccessfullyWithCachedValue() {
+        val fiscalCode = FiscalCode(TransactionTestUtils.EMAIL_STRING)
+        val fiscalCodeToken = UUID.randomUUID()
+        val computedConfidential = Confidential<FiscalCode>(fiscalCodeToken.toString())
+        fiscalCodeCachedMap[computedConfidential.opaqueData] = fiscalCode
+
+        /* test */
+        StepVerifier.create(confidentialFiscalCodeUtils.toFiscalCode(computedConfidential))
+            .expectNext(fiscalCode)
+            .verifyComplete()
+
+        verify(confidentialDataManager, Mockito.times(0)).decrypt(eq(computedConfidential), any())
+        assertEquals(fiscalCode, fiscalCodeCachedMap[computedConfidential.opaqueData])
+    }
+
+    @Test
+    fun shouldEncryptFiscalCodeSuccessfully() {
+        val fiscalCode = FiscalCode(TransactionTestUtils.EMAIL_STRING)
+        val fiscalCodeToken = UUID.randomUUID()
+        val computedConfidential = Confidential<FiscalCode>(fiscalCodeToken.toString())
+
+        /* preconditions */
+        given(confidentialDataManager.encrypt(eq(fiscalCode)))
+            .willReturn(Mono.just(computedConfidential))
+
+        /* test */
+        StepVerifier.create(confidentialFiscalCodeUtils.toConfidential(fiscalCode.value))
+            .expectNext(computedConfidential)
+            .verifyComplete()
+
+        verify(confidentialDataManager, Mockito.times(1)).encrypt(eq(fiscalCode))
+    }
+}
