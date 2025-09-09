4434bb63670a711489ec2ba082aa968798244acf
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2025-09-09 12:34:03 +0200

    chore: add commons to dependency verification (#236)

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 }
 
 // eCommerce commons library version
-val ecommerceCommonsVersion = "3.0.2"
+val ecommerceCommonsVersion = "3.2.0"
 
 // eCommerce commons library git ref (by default tag)
 val ecommerceCommonsGitRef = ecommerceCommonsVersion
