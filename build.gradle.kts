6df3954dade8f2988242920279805f66c26bd5ea
Stefano Rondinella <stefrondinella@live.it>
2026-09-01 15:17:04 +0200

    chore: update ecommerce-common version up to 3.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 }
 
 // eCommerce commons library version
-val ecommerceCommonsVersion = "3.10.1-CHK-4872-OER-logging-builder-SNAPSHOT-f1d9f08"
+val ecommerceCommonsVersion = "3.11.0"
 
 // eCommerce commons library git ref (by default tag)
 val ecommerceCommonsGitRef = ecommerceCommonsVersion
